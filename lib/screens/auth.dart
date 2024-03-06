b1befe9bc22fafb2d7676771e7892903361fb4ac
mojahid-belaman <mojahidbelaman@gmail.com>
2024-03-06 15:50:00 +0100

    create screen named AuthScreen

---
+++ lib/screens/auth.dart
@@ -0,0 +1,42 @@
+import 'package:chat_app/widgets/auth_form.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                    top: 30, right: 20, bottom: 20, left: 20),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              const Card(
+                margin: EdgeInsets.all(20),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                  child: AuthForm(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
