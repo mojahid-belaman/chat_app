4b71f96d23808f74e65b5c852364ed3aad33306a
mojahid-belaman <mojahidbelaman@gmail.com>
2024-03-07 17:21:58 +0100

    initial firebase and use methods for it to check if user is logged in or logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
-import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+import 'package:chat_app/screens/auth.dart';
 
 final theme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
         seedColor: const Color.fromARGB(255, 63, 17, 177)));
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -18,7 +27,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
       theme: theme,
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        }),
+      ),
     );
   }
 }
